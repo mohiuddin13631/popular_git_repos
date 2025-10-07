c7b4e39157447ca9954b4b276664c00596e577e9
Mohiuddin Khan <mohiuddin13631@gmail.com>
2025-10-07 13:28:33 +0600

    sorted environment

--- lib/core/util/url_container.dart
+++ lib/core/util/url_container.dart
@@ -1,7 +1,10 @@
+import 'package:popular_git_repos/core/imports/app_imports.dart';
+
 class UrlContainer {
 
-  static const String baseUrl = 'https://api.github.com/';
+  static const String domainUrl = Environment.MAIN_API_URL;
+  static const String baseUrl = '$domainUrl/search/';
 
-  static const String repositories = 'search/repositories';
+  static const String repositories = 'repositories';
 
 }
